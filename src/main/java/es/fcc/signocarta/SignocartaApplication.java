f78e1d5191dfe175a6969af94b61cfe75bc46bf2
Paqui Calatayud <paquicalatayudcastillo@gmail.com>
2025-02-03 13:21:58 +0100

    primera subida

---
+++ src/main/java/es/fcc/signocarta/SignocartaApplication.java
@@ -0,0 +1,13 @@
+package es.fcc.signocarta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignocartaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignocartaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/fcc/signocarta/SignocartaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.fcc.signocarta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignocartaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
